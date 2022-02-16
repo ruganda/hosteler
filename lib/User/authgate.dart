aa582ba6b7df0d4c019a37c455291e01d321c45b
bryanotim <43268196+bryanotim@users.noreply.github.com>
2022-02-16 10:20:00 +0300

    Create authgate.dart

---
+++ lib/User/authgate.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:hostels/user/index.dart';
+
+class AuthGate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            showAuthActionSwitch: true,
+            headerBuilder: (context, constraints, _) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset("assets/logo.jpeg"),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? 'Welcome to the Hostel\s Management! Please sign in to continue.'
+                      : 'Welcome to Hostel\s Management! Please create an account to continue',
+                ),
+              );
+            },
+            //footer
+            footerBuilder: (context, _) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions of our hostel services.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            providerConfigs: const [
+              EmailProviderConfiguration(),
+              // GoogleProviderConfiguration(
+              //   clientId: '...',
+              // ),
+            ],
+          );
+        }
+        return Index();
+      },
+    );
+  }
+}
