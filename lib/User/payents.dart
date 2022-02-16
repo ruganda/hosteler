5fd2fa59df9f23ed608ea6dda40fd1844be00e3e
Kenyc <80362426+kenyc838@users.noreply.github.com>
2022-02-16 10:36:50 +0300

    Create payents.dart

---
+++ lib/User/payents.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:flutterwave/flutterwave.dart';
+import 'package:hostels/components/palette.dart';
+
+class Payments extends StatefulWidget {
+  const Payments({Key? key}) : super(key: key);
+
+  @override
+  _PaymentsState createState() => _PaymentsState();
+}
+
+class _PaymentsState extends State<Payments> {
+  final amountController = TextEditingController();
+  final currencyController = TextEditingController();
+  final narrationController = TextEditingController();
+  final publicKeyController = TextEditingController();
+  final encryptionKeyController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  String selectedCurrency = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: fontsColor,
+        title: const Text("Payments"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: TextFormField(
+                controller: amountController,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(hintText: "Amount"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: TextFormField(
+                controller: currencyController,
+                textInputAction: TextInputAction.next,
+                style: const TextStyle(color: Colors.black),
+                readOnly: true,
+                onTap: _openBottomSheet,
+                decoration: const InputDecoration(
+                  hintText: "Currency",
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: TextFormField(
+                controller: phoneNumberController,
+                textInputAction: TextInputAction.next,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  hintText: "Phone Number",
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: TextFormField(
+                controller: this.emailController,
+                textInputAction: TextInputAction.next,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: ElevatedButton(
+                onPressed: this._onPressed,
+                style: ElevatedButton.styleFrom(primary: fontsColor),
+                child: const Text(
+                  "Make Payment",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onPressed() {
+    _handlePaymentInitialization();
+  }
+
+  _handlePaymentInitialization() async {
+    final flutterwave = Flutterwave.forUIPayment(
+        amount: amountController.text.toString().trim(),
+        currency: currencyController.text,
+        context: context,
+        publicKey: "FLWPUBK_TEST-919310eeadea8375f2576a0b91c3f252-X",
+        encryptionKey: "FLWSECK_TEST3956af9d4cbb",
+        email: emailController.text.trim(),
+        fullName: "Test User",
+        txRef: DateTime.now().toIso8601String(),
+        narration: "Example Project",
+        isDebugMode: true,
+        phoneNumber: phoneNumberController.text.trim(),
+        acceptAccountPayment: true,
+        acceptCardPayment: true,
+        acceptUSSDPayment: true);
+    final response = await flutterwave.initializeForUiPayments();
+    if (response != null) {
+      showLoading(response.toString());
+    } else {
+      showLoading("No Response!");
+    }
+  }
+
+  void _openBottomSheet() {
+    showModalBottomSheet(
+        context: this.context,
+        builder: (context) {
+          return this._getCurrency();
+        });
+  }
+
+  Widget _getCurrency() {
+    final currencies = [
+      FlutterwaveCurrency.UGX,
+      FlutterwaveCurrency.GHS,
+      FlutterwaveCurrency.NGN,
+      FlutterwaveCurrency.RWF,
+      FlutterwaveCurrency.KES,
+      FlutterwaveCurrency.XAF,
+      FlutterwaveCurrency.XOF,
+      FlutterwaveCurrency.ZMW
+    ];
+    return Container(
+      height: 250,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+      color: Colors.white,
+      child: ListView(
+        children: currencies
+            .map((currency) => ListTile(
+                  onTap: () => {this._handleCurrencyTap(currency)},
+                  title: Column(
+                    children: [
+                      Text(
+                        currency,
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                      const SizedBox(height: 4),
+                      const Divider(height: 1)
+                    ],
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  _handleCurrencyTap(String currency) {
+    setState(() {
+      selectedCurrency = currency;
+      currencyController.text = currency;
+    });
+    Navigator.pop(this.context);
+  }
+
+  Future<void> showLoading(String message) {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            margin: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+            width: double.infinity,
+            height: 50,
+            child: Text(message),
+          ),
+        );
+      },
+    );
+  }
+}
